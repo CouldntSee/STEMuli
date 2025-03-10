03e2da2685f00c56b19e5394193bb318962ab0a8
CouldntSee <leemarc469@gmail.com>
2025-03-10 13:23:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup(popupId) {
+    document.getElementById(popupId).style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
